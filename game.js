1a51c18091e160dcc57adfae29844311297320ab
FernandoQwer <skafernando2000@gmail.com>
2024-08-22 01:21:19 +0530

    Create a Spining Loader

--- game.js
+++ game.js
@@ -3,6 +3,8 @@ const choices = Array.from(document.getElementsByClassName("choice-text"));
 const progressText = document.getElementById("progressText");
 const scoreText = document.getElementById("score");
 const progressBarFull = document.getElementById("progressBarFull");
+const loader = document.getElementById('loader');
+const game = document.getElementById('game');
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -34,7 +36,7 @@ fetch("https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=mul
 
         return formattedQuestion;
     });
-    //questions = loadedQuestions;
+  
     startGame();
   })
   .catch(err => {
@@ -50,6 +52,8 @@ startGame = () => {
   score = 0;
   availableQuestions = [...questions];
   getNewQuestion();
+  game.classList.remove("hidden");
+  loader.classList.add("hidden");
 };
 
 getNewQuestion = () => {
